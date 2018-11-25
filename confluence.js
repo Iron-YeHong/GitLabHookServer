ac8e403b7a0deb2e405256e2908ad7d5cf3a3273
Iron <iron.yehong@outlook.com>
2018-11-25 17:54:28 +0800

    realized the communication between GitLab and Confluence

--- confluence.js
+++ confluence.js
@@ -1,26 +1,141 @@
-var fs = require('fs');
-// var request = require("request");
+var fs = require("fs");
+var request = require("request");
 
-// var options = {
-//   method: "GET",
-//   url: "host/rest/api/content/13893633?expand=metadata.currentuser",
-//   auth: { username: "yehong", password: "****" },
-//   headers: {
-//     Accept: "application/json"
-//   }
-// };
+var querystring = require('querystring');
+var cheerio = require("cheerio");
 
-// request(options, function(error, response, body) {
-//   if (error) throw new Error(error);
+var settings = {
+  host: "host",
+  contentId: 13893633,
+  username: "username",
+  password: "password"
+};
 
-//   JSON.stringify(data)
-//   console.log(
-//     "Response: " + response.statusCode + " " + response.statusMessage
-//   );
-//   console.log(body);
-// });
+function registerMergeRequest(merge){
+  var desc = querystring.parse(merge.description,"<<<", ">>>")
+  var mergeDesc = {
+    num:3,
+    developer: merge.developer,
+    assignee: merge.assignee,
+    changes: desc["[概述]"],
+    detail: desc["[详细说明]"],
+    influence: desc["[影响功能]"]
+  };
 
-var data = JSON.parse(fs.readFileSync('body.json'));
-console.log(data.body.editor.value)
+  updateContent(mergeDesc)
+}
 
 // https://developer.atlassian.com/cloud/confluence/rest#api-content-id-put
+function updateContent(mergeDesc) {
+  getVersion(function(version){
+    getContent(function(content) {
+      var json = JSON.parse(content);
+      var editor = buildEditor(json.body.editor.value, mergeDesc);
+  
+      json.body.editor.value = editor;
+      json.version = {
+          number: version + 1
+      }
+  
+      // 上传内容
+      var options = {
+        method: "PUT",
+        url: `${settings.host}/rest/api/content/${settings.contentId}`,
+        auth: {
+          username: `${settings.username}`,
+          password: `${settings.password}`
+        },
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(json)
+      };
+  
+      console.log("Updating contents...");
+      console.log(`Body:${options.body}`)
+  
+      request(options, function(error, response, body) {
+        if (error) throw new Error(error);
+  
+        console.log(
+          "Response: " + response.statusCode + " " + response.statusMessage
+        );
+        console.log(`Body: ${body}`);
+      });
+    });
+  })
+}
+
+function getContent(handler) {
+    console.log("Gettting contents...");
+    get("expand=body.editor", function(body){
+      console.log(`Content got!`);
+      handler(body)
+    })
+}
+
+function getVersion(handler){
+    console.log("Gettting version...");
+    get("version", function(body){
+      var json = JSON.parse(body)
+      var version = json.version.number;
+
+      handler(version)
+    })
+}
+
+function get(param, handler){
+    var options = {
+        method: "GET",
+        url: `${settings.host}/rest/api/content/${
+          settings.contentId
+        }?${param}`,
+        auth: {
+          username: `${settings.username}`,
+          password: `${settings.password}`
+        },
+        headers: {
+          Accept: "application/json"
+        }
+      };
+    
+      request(options, function(error, response, body) {
+        if (error) throw new Error(error);
+    
+        console.log(
+          "Response: " + response.statusCode + " " + response.statusMessage
+        );
+    
+        handler(body);
+      });
+}
+
+function buildEditor(old, merge) {
+//   var $ = cheerio.load(old);
+  var tr = createTableRow(merge);
+
+//   $("table thead").append(tr);
+//   return $("body").html().replace("<br>", "<br />");
+
+    var index = old.indexOf("</thead>");
+    var content = old.substr(0, index) + tr + old.substr(index);
+    
+   return content.replace(/<br>/g, '<br />');
+}
+
+function createTableRow(merge) {
+  var $ = cheerio("<tr />");
+  $.append(`<td class="confluenceTd">${merge.num}</td>`);
+  $.append(`<td colspan="1" class="confluenceTd">${merge.changes}</td>`);
+  $.append(`<td colspan="1" class="confluenceTd">${merge.developer}</td>`);
+  $.append(`<td colspan="1" class="confluenceTd">${merge.assignee}</td>`);
+  $.append(`<td colspan="1" class="confluenceTd">${merge.detail}</td>`);
+  $.append(`<td colspan="1" class="confluenceTd">${merge.influence}</td>`);
+  $.append(`<td colspan="1" class="confluenceTd"><br /></td>`);
+  $.append(`<td colspan="1" class="confluenceTd"><br /></td>`);
+  $.append(`<td colspan="1" class="confluenceTd"><br /></td>`);
+  return `<tr>${$.html()}</tr>`;
+}
+
+module.exports.register = registerMergeRequest
--- server.js
+++ server.js
@@ -1,44 +1,66 @@
 /*jshint esversion: 6 */
 
-const host = 'http://127.0.0.1';
+const host = "http://127.0.0.1";
 const port = 12701;
 
-var logger = require('./logger');
+var logger = require("./logger");
+var confluence = require("./confluence");
 
-var http = require('http');
-var handler = require('./gitlab_webhook_handler')({ path: '/webhook' });
+var http = require("http");
+var handler = require("./gitlab_webhook_handler")({ path: "/webhook" });
 
 // creat a GitLab webhook server
-http.createServer(function (req, res) {
-    handler(req, res, function () {
-        res.statusCode = 404;
-        res.end(`no such location: ${req.url}`);
+http
+  .createServer(function(req, res) {
+    handler(req, res, function() {
+      res.statusCode = 404;
+      res.end(`no such location: ${req.url}`);
     });
-}).listen(port);
+  })
+  .listen(port);
 
-logger.info(`GitLab Hook Server running at ${host}:${port}/webhook`)
+logger.info(`GitLab Hook Server running at ${host}:${port}/webhook`);
 
 // handle gitlab events
-handler.on('error', function (err) {
-    console.error('Error:', err.message);
+handler.on("error", function(err) {
+  console.error("Error:", err.message);
 });
 
-handler.on('push', function (event) {
-    console.log('Received a push event for %s to %s',
-        event.payload.repository.name,
-        event.payload.ref);
+handler.on("push", function(event) {
+  console.log(
+    "Received a push event for %s to %s",
+    event.payload.repository.name,
+    event.payload.ref
+  );
 });
 
-handler.on('issues', function (event) {
-    console.log('Received an issue event for %s action=%s: #%d %s',
-        event.payload.repository.name,
-        event.payload.action,
-        event.payload.issue.number,
-        event.payload.issue.title);
+handler.on("issues", function(event) {
+  console.log(
+    "Received an issue event for %s action=%s: #%d %s",
+    event.payload.repository.name,
+    event.payload.action,
+    event.payload.issue.number,
+    event.payload.issue.title
+  );
 });
 
-handler.on('merge', function (event) {
-    console.log('Received an merge event for %s action=%s: #%d %s',
-        event.payload.repository.name,
-        event.payload.description);
+handler.on("merge_request", function(event) {
+  if (event.payload.object_attributes.action == "open") {
+    console.log(
+      "Received an merge_request event for %s from %s",
+      event.payload.repository.name,
+      event.payload.user.name
+    );
+  } else if (event.payload.object_attributes.action == "merge") {
+    console.log(
+      "Received an merge event for %s from %s",
+      event.payload.repository.name,
+      event.payload.user.name
+    );
+    confluence.register({
+      developer: event.payload.object_attributes.last_commit.author.name,
+      assignee: event.payload.user.name,
+      description: event.payload.object_attributes.description
+    });
+  }
 });
